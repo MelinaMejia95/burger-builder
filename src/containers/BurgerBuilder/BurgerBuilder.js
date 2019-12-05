1afd375567fb34b84e156e7ae75543bede349052
melinamejia95 <melinamejia95@gmail.com>
2019-12-05 14:54:19 -0500

    Connection to Firebase data base created to store burger order info

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuilControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -78,7 +79,25 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('You continue!');
+    const order ={
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice, // On a real app, the total price should be calculated on the server
+      customer: {
+        name: 'Melina Mejía',
+        address: {
+          street: 'Teststreet 1',
+          zipCode: '1234',
+          country: 'Colombia'
+        },
+        email: 'test@test.com'
+      },
+      deliveryMethod: 'fastest'
+    };
+    axios.post('/orders.json', order)
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => console.log(error));
   }
   
   render() {
